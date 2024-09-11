452ad4d26454c9516c9c95c838209ee1972a845e
Shawn Tran <shawntran@utexas.edu>
2024-09-10 22:43:40 -0500

    push to portfolio repo

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -14,14 +14,14 @@ function Main() {
         <div className="content">
           <div className="social_icons">
             <a href="https://github.com/yujisatojr" target="_blank" rel="noreferrer"><GitHubIcon/></a>
-            <a href="https://www.linkedin.com/in/yujisato/" target="_blank" rel="noreferrer"><LinkedInIcon/></a>
+            <a href="https://www.linkedin.com/in/shawntran/" target="_blank" rel="noreferrer"><LinkedInIcon/></a>
           </div>
-          <h1>Yuji Sato</h1>
-          <p>Full Stack Engineer</p>
+          <h1>Shawn Tran</h1>
+          <p>Data Scientist / Continuous Improvement Engineer</p>
 
           <div className="mobile_social_icons">
             <a href="https://github.com/yujisatojr" target="_blank" rel="noreferrer"><GitHubIcon/></a>
-            <a href="https://www.linkedin.com/in/yujisato/" target="_blank" rel="noreferrer"><LinkedInIcon/></a>
+            <a href="https://www.linkedin.com/in/shawntran/" target="_blank" rel="noreferrer"><LinkedInIcon/></a>
           </div>
         </div>
       </div>
